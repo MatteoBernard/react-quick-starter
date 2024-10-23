b995934cbe2dfd405701b991c4bed43826594216
Bernard Mattéo <matteo_bernard1@ens.univ-artois.fr>
2024-10-24 01:46:40 +0200

    Install React i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {HashRouter} from "react-router-dom";
 import RootNavigation from "./routing/RootNavigation";
+import {useTranslation} from "react-i18next";
 
 export const App = () => {
+
+    const { t } = useTranslation();
+
     return (
         <HashRouter>
+            <h1>{t("hello")}</h1>
             <RootNavigation />
         </HashRouter>
     );
